ed39d5ffbd2668f06f8224724c879e75b02574af
James Wolff <james.wolff0@gmail.com>
2015-04-26 19:48:23 -0500

    Removed some debug code and added support for Plugin Metrics

--- src/io/github/jwolff52/livebroadcast/LiveBroadcast.java
+++ src/io/github/jwolff52/livebroadcast/LiveBroadcast.java
@@ -24,6 +24,7 @@ import io.github.jwolff52.livebroadcast.util.SettingsManager;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.logging.Logger;
@@ -36,6 +37,7 @@ import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.mcstats.Metrics;
 
 public final class LiveBroadcast extends JavaPlugin {
 	public final Logger logger = Logger.getLogger("Minecraft");
@@ -118,6 +120,13 @@ public final class LiveBroadcast extends JavaPlugin {
 			}
 		}, 0L, getTimer());
 		/*************************************************************/
+		
+		try {
+			Metrics metrics = new Metrics(this);
+			metrics.start();
+		} catch (IOException e) {
+			logger.warning("[LiveBroadcast] There was an error starting plugin metrics!");
+		}
 		this.logger.info(pdf.getName() + " Version: " + pdf.getVersion()
 				+ " has been enabled!");
 		super.onEnable();
@@ -385,13 +394,11 @@ public final class LiveBroadcast extends JavaPlugin {
 	public long getTimer() {
 		if(useScalableTimer) {
 			int time = (int)(maxTime - (maxTime * (getServer().getOnlinePlayers().size()/(double)maxPlayers)));
-			logger.info(time+"");
 			if(time < minTime) {
 				return minTime;
 			}
 			return time;
 		} else {
-			logger.info("blah");
 			return minTime;
 		}
 	}
